1de5ff52a97ed692f6ff47258f5d0343daaa06cd
AndreyGorban <Whiteman1989@gmail.com>
2021-04-10 19:00:18 +0300

    Added magnet block

---
+++ Assets/Scripts/Magnet.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public class Magnet : MonoBehaviour
+{
+    [Header("Gameplay options")]
+    [SerializeField]
+    private bool isEnable = false;
+    [SerializeField]
+    private int timeDelay = 0;
+
+    [Header("FX options")]
+    [SerializeField]
+    private Sprite enableSprite;
+    private Sprite disableSprite;
+    [SerializeField]
+    private ParticleSystem particleSystem;
+
+
+    private PointEffector2D pointEffector;
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointEffector = GetComponent<PointEffector2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        disableSprite = spriteRenderer.sprite;
+        particleSystem = GetComponentInChildren<ParticleSystem>();
+        if(isEnable)
+        {
+            EnableMagnet();
+        }
+        else
+        {
+            DisableMagnet();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeDelay > 0 && !isEnable)
+        {
+            WaitAndRun();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        DisableMagnet();
+    }
+
+    private async void AsingTest()
+    {
+        await Task.Delay(3000);
+        Debug.LogWarning("Task ready");
+    }
+
+    private async void WaitAndRun()
+    {
+        isEnable = true;
+        await Task.Delay(timeDelay);
+        EnableMagnet();
+
+    }
+
+    public void EnableMagnet()
+    {
+        spriteRenderer.sprite = enableSprite;
+        pointEffector.enabled = true;
+        particleSystem.Play();
+    }
+
+    public void DisableMagnet()
+    {
+        spriteRenderer.sprite = disableSprite;
+        pointEffector.enabled = false;
+        particleSystem.Stop();
+        isEnable = false;
+    }
+}
